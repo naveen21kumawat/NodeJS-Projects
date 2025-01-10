561be93f622e37ff5986f70955f43ea51065de4f
Naveen Kumawat <naveendimond77@gmail.com>
2025-01-10 11:45:14 +0530

    Done

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-// fs.writeFile("ou.txt","File Written ",(e)=> console.log("H"))
+fs.writeFile("ou.txt","File Written ",(e)=> console.log("error occured"))
 fs.writeFile("output.txt","Writing file success",(err) =>{  if (err) console.log("Error Uccured")
   else console.log("File written succeful")
 })
