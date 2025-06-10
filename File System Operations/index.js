01fda5bc1695482ae9c5e5d5ff329313bb5c3343
Naveen Kumawat <naveendimond77@gmail.com>
2025-06-10 13:37:09 +0530

    Add initial implementation of file system operations

---
+++ File System Operations/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+// fs.writeFile('example.txt', 'Hello, World!', (err) => {
+//   if (err) {
+//     console.error('Error writing to file:', err);
+//   } else {
+//     console.log('File written successfully');
+//   }
+// })
+
+// fs.appendFile('example.txt', 'Hello, World! ', (err) => {
+//   if (err) {
+//     console.error('Error writing to file:', err);
+//   } else {
+//     console.log('File written successfully');
+//   }
+// })
+
+// fs.rename('example.txt', 'example2.txt', (err) => {
+//   if (err) {
+//     console.error('Error renaming file:', err);
+//   } else {
+//     console.log('File renamed successfully');
+//   }
+// })
+fs.copyFile('example2.txt', './copy/example3.txt', (err) => {
+  if (err) {
+    console.error('Error copying file:', err.message);
+  } else {
+    console.log('File copied successfully');
+  }
+});
