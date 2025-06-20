e5713ab82ee59554d72f23885a9524c9c06647e4
Naveen Kumawat <naveendimond77@gmail.com>
2025-06-21 00:10:19 +0530

    Implement file listing and task creation functionality with EJS view updates

--- Practise Set/Practice Set1/index.js
+++ Practise Set/Practice Set1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fstat = require('fs');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -9,7 +10,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-  res.render("index");
+  fstat.readdir(`./files`,(err,files)=>{
+    res.render("index",{files: files});
+  })
 })
 
+app.post('/create',(req,res)=>{
+//  fstat.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.content,(err)=>{
+//   console.log("chal jljd");
+//  })
+//   res.redirect('/'); 
+console.log("create page")
+fstat.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.content,(err)=>{
+ 
+}
+)
+  res.redirect('/');}
+)
 app.listen(3000)
