25f60cb0af76597c5152a15fa1027b01275518eb
Naveen Kumawat <naveendimond77@gmail.com>
2025-06-24 21:09:05 +0530

    Update

--- Practise Set/calculator/CalculatorSetup.js
+++ Practise Set/calculator/CalculatorSetup.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const calReq = require('./Calculator')
 const server = http.createServer(calReq);
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
