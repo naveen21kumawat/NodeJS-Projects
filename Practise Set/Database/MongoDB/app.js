89bac5dc189bfb4bafd9d6f97abaec24519bb43d
Naveen Kumawat <naveendimond77@gmail.com>
2025-06-25 22:48:45 +0530

    Refactor app.js for consistent code style and formatting

--- Practise Set/Database/MongoDB/app.js
+++ Practise Set/Database/MongoDB/app.js
@@ -1,20 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userModel = require('./userModel');
+const userModel = require("./userModel");
 
-app.get('/', (req, res) => {
-  res.send('Hello, MongoDB!');
+app.get("/", (req, res) => {
+  res.send("Hello, MongoDB!");
 });
 
-app.get('/create',async (req,res)=>{
+app.get("/create", async (req, res) => {
   let createduser = await userModel.create({
-    name:'Naveen',
-    username:'naveen121kumawat',
-    email:'naveen21kumawat@gmail.com'
-  })
+    name: "Naveen",
+    username: "naveen121kumawat",
+    email: "naveen21kumawat@gmail.com",
+  });
   res.send(createduser);
-})
+});
+app.get("/update", async (req, res) => {
+  let updateduser = await userModel.findOneAndUpdate(
+    { username: "naveen" },
+    { username: "naveen21kumawat" }
+  );
+  res.send(updateduser);
+});
 
-app.listen(3000,()=>{
-  console.log('Server is running on port 3000');
-})
+app.get("/read", async (req, res) => {
+  let users = await userModel.find();
+  res.send(users);
+});
+// app.get("/read", async (req, res) => {
+//   let users = await userModel.find({ name: "Naveen" });
+//   res.send(users);
+// });
+app.get("/delete", async (req, res) => {
+  let deleteduser = await userModel.findOneAndDelete({
+    name: "naveen21",
+  });
+  res.send(deleteduser);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
