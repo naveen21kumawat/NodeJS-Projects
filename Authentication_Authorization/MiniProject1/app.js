dc6799ab7646e6a95408675ed1e208b2d685a890
Naveen Kumawat <naveendimond77@gmail.com>
2025-07-13 19:43:30 +0530

    Login Aythentication

--- Authentication_Authorization/MiniProject1/app.js
+++ Authentication_Authorization/MiniProject1/app.js
@@ -16,7 +16,8 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/profile", isLoggedIn, (req, res) => {
-  // res.send("profile")
+  res.send("Login Successfull");
+  console.log("User Data", req.user);
 });
 
 app.get("/login", (req, res) => {
@@ -38,30 +39,29 @@ app.post("/create", async (req, res) => {
       });
       const token = jwt.sign({ email: email, userId: user._id }, "shhhh");
       res.cookie("token", token);
-      res.send("Registered");
     });
+    res.redirect("/login");
   });
-  console.log(name);
 });
 
 app.post("/login", async (req, res) => {
   let { email, password } = req.body;
   let user = await userModel.findOne({ email });
-  if (!user) return res.status(500).send("Something went wrong");
+  if (!user) return res.status(500).send("Password or email is incorrect");
 
   bcrypt.compare(password, user.password, (err, result) => {
     if (result) {
       const token = jwt.sign({ email: email, userId: user._id }, "shhhh");
       res.cookie("token", token);
-      res.status(200).send("you can Login");
-      // res.redirect('/profile')
+      // res.status(200).send("you can Login");
+      res.redirect("/profile");
     } else res.status(500).send("Something went wrong");
   });
 });
 
 app.get("/logout", (req, res) => {
-  res.cookie('token',"");
-  res.redirect("/login");
+  res.cookie("token", "");
+  // res.redirect("/login");
 });
 
 function isLoggedIn(req, res, next) {
@@ -70,7 +70,8 @@ function isLoggedIn(req, res, next) {
     return res.status(401).send("You are not logged in");
   } else {
     let data = jwt.verify(token, "shhhh");
-    console.log(data)
+    req.user = data;
+    next();
   }
 }
 
